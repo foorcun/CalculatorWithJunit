3b491cc5fa669d829c070d2b06b54243536b2520
User <User@192.168.1.5>
2021-01-17 18:38:14 +0200

    first push

---
+++ CalculatorWithJunit/src/main/java/com/troyaTech/CalculatorWithJunit/CalculatorWithJunitApplication.java
@@ -0,0 +1,13 @@
+package com.troyaTech.CalculatorWithJunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorWithJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorWithJunitApplication.class, args);
+	}
+
+}
---
+++ CalculatorWithJunit/src/test/java/com/troyaTech/CalculatorWithJunit/CalculatorWithJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.troyaTech.CalculatorWithJunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorWithJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
